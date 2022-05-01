6e0a4e2c671c4e44c9ea4295141586868b3ba1d9
mcavolowsky <mark.cavolowsky@gmail.com>
2022-05-01 18:39:17 -0400

    Working on the refactoring to use rl_starter_files.

--- gym_minigrid/envs/__init__.py
+++ gym_minigrid/envs/__init__.py
@@ -21,4 +21,4 @@ from gym_minigrid.envs.dynamicobstacles import *
 from gym_minigrid.envs.distshift import *
 
 
-from gym_minigrid.envs.multi_stl_env import *
+#from gym_minigrid.envs.multi_stl_env import *
